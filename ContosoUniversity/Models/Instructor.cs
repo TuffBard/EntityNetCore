1838805f953caaad161399053dcb7a6966d3ce01
Nicolas Garcia <tuffbard@yopmail.fr>
2017-02-01 17:34:26 +0100

    Add Instructor Entity

---
+++ ContosoUniversity/Models/Instructor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class Instructor
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50,ErrorMessage = "The name is too long")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The first name is too long")]
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date), DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name ="Hire Date")]
+        public DateTime HireDate { get; set; }
+
+        [Display(Name ="Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+
+        public ICollection<CourseAssignment> Courses { get; set; }
+        public OfficeAssignment OfficeAssignment { get; set; }
+    }
+}
--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -9,22 +9,37 @@ namespace ContosoUniversity.Models
     {
         public int ID { get; set; }
 
-        //Le nom ne doit pas dépasser 50 characteres
-        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 chars.")]
+        //Le champ nom est requis
+        [Required]
+        //Le nom ne doit être compris entre 2 et 50 characteres
+        [StringLength(50, MinimumLength = 2,ErrorMessage = "Last name cannot be longer than 50 chars.")]
         //Le nom doit commencer pas une majuscule et être suivis de lettre minuscule
         [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$", ErrorMessage ="The first character must be uppercase. Only letter are allowed.")]
+        //Définis le nom affiché de la colonne
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
-
-        //Le prénom ne doit pas dépasser 50 characteres
+        
+        [Required]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 chars.")]
         //La colonne FirstMidName est renommée en FirstName
         [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
 
         [DataType(DataType.Date)]
         //Force le format d'affichage de la date.
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name ="Enrollment Name")]
         public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name ="Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
