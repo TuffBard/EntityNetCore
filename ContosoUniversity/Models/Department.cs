4d3127053a3c84a364e89b17fcaf6fbec8fdf4c5
Nicolas Garcia <tuffbard@yopmail.fr>
2017-02-02 10:40:58 +0100

    Create a complex data model

---
+++ ContosoUniversity/Models/Department.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+        
+        public int Budget { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName ="Start Date")]
+        public DateTime StartDate { get; set; }
+
+        public int? InstructorID { get; set; }
+
+        public Instructor Administrator { get; set; }
+        public ICollection<Course> Courses { get; set; }
+    }
+}
